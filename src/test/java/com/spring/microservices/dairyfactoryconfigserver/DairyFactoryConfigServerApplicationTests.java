c68339eba5027cdab2ee24ae38d56b434ab7168d
Mohanavelu Subramanian <mhnvelu@gmail.com>
2021-06-03 18:55:42 +0100

    Added Spring Cloud Config Server

---
+++ src/test/java/com/spring/microservices/dairyfactoryconfigserver/DairyFactoryConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.microservices.dairyfactoryconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyFactoryConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
